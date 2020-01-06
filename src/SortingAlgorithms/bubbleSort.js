0015532c5696b551b6d00ad8d2853d8687e294f8
ryanliu18 <ryanliu@Ryans-MacBook-Pro.local>
2020-01-05 19:24:41 -0800

    added single pass boolean for bubble sort

--- src/SortingAlgorithms/bubbleSort.js
+++ src/SortingAlgorithms/bubbleSort.js
@@ -1,6 +1,7 @@
 export function getBubbleSortAnimations(array) {
 
     const animations = [];
+    var swapped = false;
 
     for (let i = 0; i < array.length-1; i++) {
         for (let j = 0; j < array.length-i-1; j++) {
@@ -12,8 +13,10 @@ export function getBubbleSortAnimations(array) {
                 var temp = array[j];
                 array[j] = array[j+1];
                 array[j+1] = temp;
+                swapped = true;
             }
         }
+        if (!swapped) break;
     }
 
     return animations;
